f1753fc7ca667edbda24218c2657854f68ee8146
scientistmind <luk.ku@hotmail.com>
2020-07-06 17:12:31 +0530

    first commit

---
+++ app/route.js
@@ -0,0 +1,19 @@
+var app = angular.module("myApp", ["ngRoute"]);
+app.config(function($routeProvider) {
+    $routeProvider
+    .when("/", {
+        templateUrl : "main.html"
+    })
+    .when("/data", {
+        templateUrl : "api/data.php"
+    })
+   
+});
+// function getResultsPage(){
+//     $http({
+//         url: URL + 'api/data.php',
+//         method: 'GET'
+//      }).then(function(res){
+//         $scope.data = res.data.data;
+//      });
+// }
